48c27259c85dff0826c15b7a34dcba8ff6fac882
민경원 <76027752+sojoonghan2@users.noreply.github.com>
2024-11-17 20:40:35 +0900

    [min] 애니메이션 관련 주석 추가

--- Engine/Animator.cpp
+++ Engine/Animator.cpp
@@ -21,14 +21,18 @@ void Animator::FinalUpdate()
 {
 	_updateTime += DELTA_TIME;
 
+	// 몇 번째 클립?
 	const AnimClipInfo& animClip = _animClips->at(_clipIndex);
 	if (_updateTime >= animClip.duration)
 		_updateTime = 0.f;
 
 	const int32 ratio = static_cast<int32>(animClip.frameCount / animClip.duration);
+	// 현재 프레임
 	_frame = static_cast<int32>(_updateTime * ratio);
 	_frame = min(_frame, animClip.frameCount - 1);
+	// 다음 프레임
 	_nextFrame = min(_frame + 1, animClip.frameCount - 1);
+	// 1과 2프레임 사이에 있는 애매한 1.6프레임 같은 애들을 frameRatio라고 함
 	_frameRatio = static_cast<float>(_frame - _frame);
 }
 
@@ -43,7 +47,6 @@ void Animator::PushData()
 	if (_boneFinalMatrix->GetElementCount() < boneCount)
 		_boneFinalMatrix->Init(sizeof(Matrix), boneCount);
 
-	// Compute Shader
 	shared_ptr<Mesh> mesh = GetGameObject()->GetMeshRenderer()->GetMesh();
 	mesh->GetBoneFrameDataBuffer(_clipIndex)->PushComputeSRVData(SRV_REGISTER::t8);
 	mesh->GetBoneOffsetBuffer()->PushComputeSRVData(SRV_REGISTER::t9);
@@ -58,7 +61,6 @@ void Animator::PushData()
 	uint32 groupCount = (boneCount / 256) + 1;
 	_computeMaterial->Dispatch(groupCount, 1, 1);
 
-	// Graphics Shader
 	_boneFinalMatrix->PushGraphicsData(SRV_REGISTER::t7);
 }
 
