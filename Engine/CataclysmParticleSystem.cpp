1532e994113bcc3ab7213c951f8dcf988ebf839f
KyeongWonMin(laptop) <76027752+sojoonghan2@users.noreply.github.com>
2025-01-07 10:11:38 +0900

    대격변 파티클 파일 추가

---
+++ Engine/CataclysmParticleSystem.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "FireParticleSystem.h"
+
+FireParticleSystem::FireParticleSystem() : BaseParticleSystem()
+{
+	SetParticleInterval(0.01f, 0.0f);
+	SetParticleLifeTime(3.0f, 5.0f);
+	SetParticleSpeed(50.0f, 30.0f);
+	SetParticleScale(10.0f, 50.0f);
+	SetDuration(2.0f);
+	SetMaterial(GET_SINGLE(Resources)->Get<Material>(L"FireParticle")->Clone(), GET_SINGLE(Resources)->Get<Material>(L"FireComputeParticle")->Clone());
+	shared_ptr<Texture> texture = GET_SINGLE(Resources)->Load<Texture>(L"FireParticleTexture", L"..\\Resources\\Texture\\Particle\\fire.png");
+	SetParticleTexture(texture);
+}
+
+void FireParticleSystem::FinalUpdate()
+{
+	BaseParticleSystem::FinalUpdate();
+}
