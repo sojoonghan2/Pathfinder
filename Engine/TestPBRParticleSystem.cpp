eaf370431651c9b3e2fd4f24866b5d2e6866d378
KyeongWonMin(laptop) <76027752+sojoonghan2@users.noreply.github.com>
2025-01-09 14:37:18 +0900

    PBR 테스트용 파티클 테스트 쉽게 수정

--- Engine/TestPBRParticleSystem.cpp
+++ Engine/TestPBRParticleSystem.cpp
@@ -3,13 +3,13 @@
 
 TestPBRParticleSystem::TestPBRParticleSystem() : BaseParticleSystem()
 {
-	SetParticleInterval(0.001f, 0.0f);
-	SetParticleLifeTime(3.0f, 5.0f);
+	SetParticleInterval(0.9999999f, 0.0f);
+	SetParticleLifeTime(5.0f, 5.0f);
 	SetParticleSpeed(0.0f, 0.0f);
 	SetParticleScale(100.0f, 100.0f);
 	SetDuration(1000.0f);
 	SetOnceParticleNum(1);
-	SetMaterial(GET_SINGLE(Resources)->Get<Material>(L"TestPBRParticle")->Clone(), GET_SINGLE(Resources)->Get<Material>(L"CataclysmComputeParticle")->Clone());
+	SetMaterial(GET_SINGLE(Resources)->Get<Material>(L"TestPBRParticle")->Clone(), GET_SINGLE(Resources)->Get<Material>(L"TestPBRComputeParticle")->Clone());
 	shared_ptr<Texture> texture = GET_SINGLE(Resources)->Load<Texture>(L"TestPBRComputeParticle", L"..\\Resources\\Texture\\Particle\\tmp.png");
 	SetParticleTexture(0, texture);
 }
